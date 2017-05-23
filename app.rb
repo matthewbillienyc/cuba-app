4c4b3f260928011526bb165bad8e89f9ecd46fa2
matthewbillienyc <matthewbillienyc@gmail.com>
2017-05-23 14:01:07 -0400

    integrate with redis, post and list fun things

--- app.rb
+++ app.rb
@@ -3,26 +3,38 @@ require 'cuba/render'
 require 'tilt/haml'
 require 'byebug'
 require 'rack'
-require 'rack/static'
 require 'haml'
+require 'redis'
 
 Cuba.plugin Cuba::Render
-
-Cuba.use Rack::Static,
-         urls: ['css']
 
 Cuba.settings[:render][:template_engine] = 'haml'
 Cuba.settings[:render][:layout] = 'layout'
 
 Cuba.define do
+  redis = Redis.new
+
   on get do
     on root do
+      @fun_things = redis.keys.each_with_object({}) do |k, h|
+        h[k] = redis.get k
+      end
+      
       res.write view('landing')
     end
 
     on 'public/css', extension('css') do
       res['Content-Type'] = 'text/css'
       res.write File.read(".#{req.path}")
+    end
+  end
+
+  on post do
+    on 'new_thing'  do
+      on param('thing'), param('description') do |thing, desc|
+        redis.set(thing, desc)
+        res.redirect('/')
+      end
     end
   end
 end
